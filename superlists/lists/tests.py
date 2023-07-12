b7ef0aba142f842df07966ba96ed7d2ee8035cec
Hevannder Rubian <hevannder.albuquerque@fpf.br>
2023-07-11 23:33:29 -0400

    chapter 3 - reading html response and failing functional tests

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,3 +1,4 @@
+from django.http import HttpRequest
 from django.test import TestCase
 from . import views
 from django.urls import resolve
@@ -8,6 +9,14 @@ class HomePagetest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, views.home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = views.home_page(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertTrue(response.content.endswith(b'</html>'))
+        self.assertIn(b'<title>To-Do lists</title>', response.content)
+
 
 
 # run with python manage.py test
--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
-def home_page():
-    return 'hello world'
+def home_page(request):
+    page = '<html><title>To-Do lists</title></html>'
+    response = HttpResponse(page)
+    print(type(response.content))
+    return response
 
 
 def second_page():
